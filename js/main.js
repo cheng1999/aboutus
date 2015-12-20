c3e7131b3f10851ebb7f36a352235a30e34621c3
Lee Guo Cheng <linuxer17@gmail.com>
2015-12-20 18:02:35 +0800

    github.io page availabel

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$("img").on('click mouseover', function (){
+    var index=$(this).attr('id').replace("img-","");
+    index=parseInt(index);
+    $("img").css('border','0');
+    $(this).css('border','5px solid');
+    $(".profile").hide();
+    $(".profile").eq(index).show();
+    $(".arrow").css('opacity','0');
+    $(".arrow").eq(index).css('opacity','1');
+});
+
+
