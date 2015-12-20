436039d09aff8d137dea467781e7c016a41440f6
hang333 <jonathan.goh333@gmail.com>
2015-12-20 21:41:41 +0800

    Add some animation effect.

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 $("img").on('click mouseover', function (){
     var index=$(this).attr('id').replace("img-","");
     index=parseInt(index);
-    $("img").css('border','0');
-    $(this).css('border','5px solid');
+    //$("img").css('border','0');
+    //$(this).css('border','5px solid');
     $(".profile").hide();
     $(".profile").eq(index).show();
     $(".arrow").css('opacity','0');
