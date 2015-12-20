4b185844fa19ea4c89c54d05b7b4e24d69cef960
Lee Guo Cheng <linuxer17@gmail.com>
2015-12-20 16:20:52 +0800

    format few js code

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ $("img").on('click mouseover', function (){
     $("img").css('border','0');
     $(this).css('border','5px solid');
     $(".profile").hide();
-	$(".profile").eq(index).show();
+    $(".profile").eq(index).show();
     $(".arrow").css('opacity','0');
-	$(".arrow").eq(index).css('opacity','1');
+    $(".arrow").eq(index).css('opacity','1');
 });
 
 
