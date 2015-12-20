fe36eef7c55822ef7f6b8d8dc6dc5d2868655981
Lee Guo Cheng <linuxer17@gmail.com>
2015-12-20 20:39:16 +0800

    add some about profile XD and scroll top function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 $("img").on('click mouseover', function (){
     var index=$(this).attr('id').replace("img-","");
     index=parseInt(index);
@@ -9,4 +10,20 @@ $("img").on('click mouseover', function (){
     $(".arrow").eq(index).css('opacity','1');
 });
 
+$("#img-0").click();
 
+
+//copy from http://codepen.io/rdallaire/pen/apoyx
+// ===== Scroll to Top ====
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
